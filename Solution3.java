08c0f74cde6afb9d699f5aff0692f4ec5900352e
JiaYipeng <2312386498@qq.com>
2025-11-03 22:02:12 +0800

    Solution3已经纠正

--- Solution3.java
+++ Solution3.java
@@ -1,73 +1,54 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-/**
- * @description:
- *
- * 给你一个由 无重复 正整数组成的集合 nums ，请你找出并返回其中最大的整除子集 answer ，子集中每一元素对 (answer[i], answer[j]) 都应当满足：
- * answer[i] % answer[j] == 0 ，或
- * answer[j] % answer[i] == 0
- * 如果存在多个有效解子集，返回其中任何一个均可。
- *
- *
- *
- * 示例 1：
- *
- * 输入：nums = [1,2,3]
- * 输出：[1,2]
- * 解释：[1,3] 也会被视为正确答案。
- * 示例 2：
- *
- * 输入：nums = [1,2,4,8]
- * 输出：[1,2,4,8]
- *
- *
- * 提示：
- *
- * 1 <= nums.length <= 1000
- * 1 <= nums[i] <= 2 * 109
- * nums 中的所有整数 互不相同
- *
- */
-class Solution3 {
+public class Solution3 {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int len = nums.length-1;
+        int len = nums.length;
+        if (len == 0) return new ArrayList<>();
+
+        // 去除重复元素并排序
+        Set<Integer> uniqueNums = new HashSet<>();
+        for (int num : nums) {
+            uniqueNums.add(num);
+        }
+        nums = uniqueNums.stream().mapToInt(Integer::intValue).toArray();
         Arrays.sort(nums);
 
         // 第 1 步：动态规划找出最大子集的个数、最大子集中的最大整数
-        int[] dp = new int[len];
-        Arrays.fill(dp, 1);
-        int maxSize = 1;
-        int maxVal = dp[0];
-        for (int i = 1; i < len; i++) {
-            for (int j = 1; j < i; j++) {
-                // 题目中说「没有重复元素」很重要
-                if (nums[i] % nums[j] == 0) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int[] dp = new int[nums.length];
+        int[] prev = new int[nums.length];  // 用来记录链中的前一个元素
+        Arrays.fill(dp, 1);  // 每个元素至少能包含自己
+        Arrays.fill(prev, -1);  // 初始时没有前驱元素
+        int maxSize = 1;  // 最大子集的大小
+        int maxValIndex = 0;  // 最大子集的末尾元素的索引
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 如果 nums[i] 能被 nums[j] 整除且能形成更大的子集
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;  // 更新子集大小
+                    prev[i] = j;  // 记录链中的前一个元素
                 }
             }
-
             if (dp[i] > maxSize) {
                 maxSize = dp[i];
-                maxVal = i;
+                maxValIndex = i;  // 更新最大子集末尾的元素索引
             }
         }
 
         // 第 2 步：倒推获得最大子集
-        List<Integer> res = new ArrayList<Integer>();
-        if (maxSize == 1) {
-            res.add(nums[0]);
-            return res;
+        List<Integer> res = new ArrayList<>();
+        while (maxValIndex != -1) {
+            res.add(nums[maxValIndex]);
+            maxValIndex = prev[maxValIndex];  // 通过 prev 数组倒推
         }
 
-        for (int i = len - 1; i >= 0; i--) {
-            if (dp[i] == maxSize && maxVal % nums[i] == 0) {
-                res.add(nums[i]);
-                maxVal = nums[i];
-                maxSize--;
-            }
-        }
+        // 因为是倒推的结果，需要反转
+        Collections.reverse(res);
         return res;
     }
 }
